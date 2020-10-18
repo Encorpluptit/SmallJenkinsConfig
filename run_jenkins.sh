476c61a99e46fd8c3ce526b8158cfd6e7dee6a29
Clément Doucy <clement.doucy@epitech.eu>
2020-10-18 16:59:05 +0200

    feat : add script to run jenkins using docker and emptu my_marvin.yml

---
+++ run_jenkins.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+docker run \
+    -d \
+    -p 8080:8080 \
+    -p 50000:50000 \
+    -v "jenkins_home:/var/jenkins_home" \
+    -v "$PWD/my_marvin.yml:/var/jenkins_home/my_marvin.yml"
+    --env CASC_JENKINS_CONFIG=/var/jenkins_home/my_marvin.yml \
+    --name my_marvin \
+    jenkins/jenkins:latest
+
+docker logs -f my_marvin

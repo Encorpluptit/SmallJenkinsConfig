dbf6aa052860f800851d609e8ae2cbaf67318861
Clément Doucy <clement.doucy@epitech.eu>
2020-10-18 17:45:31 +0200

    Feat : add users and authorization

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -7,6 +7,10 @@ docker run \
     -v "jenkins_home:/var/jenkins_home" \
     -v "$PWD/my_marvin.yml:/var/jenkins_home/my_marvin.yml" \
     --env CASC_JENKINS_CONFIG=/var/jenkins_home/my_marvin.yml \
+    --env USER_CHOCOLATEEN_PASSWORD=a \
+    --env USER_VAUGIE_G_PASSWORD=a \
+    --env USER_I_DONT_KNOW_PASSWORD=a \
+    --env USER_NASSO_PASSWORD=a \
     --name my_marvin \
     jenkins/jenkins:latest
 

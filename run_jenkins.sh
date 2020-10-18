df8468617e2308497c591a341723ecbc1d1bc80b
Clément Doucy <clement.doucy@epitech.eu>
2020-10-18 16:59:42 +0200

    feat : syntax in run.jenkins.sh

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -5,7 +5,7 @@ docker run \
     -p 8080:8080 \
     -p 50000:50000 \
     -v "jenkins_home:/var/jenkins_home" \
-    -v "$PWD/my_marvin.yml:/var/jenkins_home/my_marvin.yml"
+    -v "$PWD/my_marvin.yml:/var/jenkins_home/my_marvin.yml" \
     --env CASC_JENKINS_CONFIG=/var/jenkins_home/my_marvin.yml \
     --name my_marvin \
     jenkins/jenkins:latest
